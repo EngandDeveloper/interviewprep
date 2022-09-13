2a26f51445a196a318b18eea5b0b236f13f559cd
EngandDeveloper <46913754+EngandDeveloper@users.noreply.github.com>
2022-09-12 23:16:25 -0400

    Create even_first_array.py

---
+++ even_first_array.py
@@ -0,0 +1,9 @@
+''' 
+*** Problem is from the book Elements Of Programming Interview With Python Chapter 5: Arrays
+*** Problem Definition ***
+Your input is an array of
+integers, and you have to reorder its entries so that the even entries appear first. 
+This is easy if you use O(n) space, where n is the length of the array. 
+However, you are required to solve it without allocating additional storage.
+'''
+
