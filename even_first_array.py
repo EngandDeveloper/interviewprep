f8e10645dc9fb3f66749c00572609383d16784f0
EngandDeveloper <46913754+EngandDeveloper@users.noreply.github.com>
2022-09-13 17:09:10 -0400

    Update even_first_array.py

--- even_first_array.py
+++ even_first_array.py
@@ -7,3 +7,12 @@ This is easy if you use O(n) space, where n is the length of the array.
 However, you are required to solve it without allocating additional storage.
 '''
 
+def reorder_array(arr):
+  even_index = 0
+  odd_index = len(arr) - 1
+  while even_index < odd_index:
+    if arr[even_index] % 2 == 0:
+      even_index += 1
+    else:
+      arr[even_index], arr[odd_index] = arr[odd_idex], arr[even_index]
+      odd_index -= 1
